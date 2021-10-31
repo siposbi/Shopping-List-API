9034f0f51692b2a537cf69fd1afb73fc71899d14
Sipos Balázs <balazs.sipos@ns-innovations.com>
2021-10-31 09:09:01 +0100

    Added async migration on startup

--- SharedShoppingList.Api/Program.cs
+++ SharedShoppingList.Api/Program.cs
@@ -1,13 +1,23 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SharedShoppingList.Data;
 
 namespace SharedShoppingList.Api
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ShoppingListDbContext>();
+            await context.Database.MigrateAsync();
+
+            await host.RunAsync();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
